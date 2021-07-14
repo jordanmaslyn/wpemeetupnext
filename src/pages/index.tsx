bda54bac6b0ee7a2fd1bdd81736ba78be1a64963
Jordan Maslyn <jordan.maslyn@wpengine.com>
2021-07-14 16:18:16 -0400

    Filter out users w/o location and add location to list content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,15 @@ import { client, RootQueryToUserConnectionEdge } from 'client';
 export default function Page() {
   const { users } = client.useQuery();
   const isNotExcluded = ({ node }: RootQueryToUserConnectionEdge) => !node.meetupInfo.exclude;
-  const displayedUsers = users().edges.filter(isNotExcluded);
+  const hasLocation = ({ node }: RootQueryToUserConnectionEdge) => node.meetupInfo?.location != null;
+  const displayedUsers = users().edges.filter(isNotExcluded).filter(hasLocation);
 
   return (
     <AuthorizedContent>
       <ul>
         {displayedUsers.map(({ node }, index) => {
           return (
-            <li key={node?.id ?? index}>{node.nicename}</li>
+            <li key={node?.id ?? index}>{node.firstName} {node.lastName} - {node.meetupInfo.location.city}, {node.meetupInfo.location.state}</li>
           )
         })}
       </ul>
