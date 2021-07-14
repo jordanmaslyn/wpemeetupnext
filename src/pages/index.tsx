f5cb5ed6ea486527b30cbb6390a09ccee3098f7b
Jordan Maslyn <jordan.maslyn@wpengine.com>
2021-07-14 12:37:33 -0400

    List non admin users on front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { AuthorizedContent } from 'components/AuthorizedContent';
+import { client } from 'client';
+import { RootQueryToUserConnectionEdge } from '@faustjs/core';
 
 export default function Page() {
+  const { users } = client.useQuery();
+  const isNotAdmin = ({ node }: RootQueryToUserConnectionEdge) => !node.roles().nodes.map(n => n.name).includes('administrator');
+  const displayedUsers = users().edges.filter(isNotAdmin);
 
   return (
     <AuthorizedContent>
-      <h1>You are successfully logged in!</h1>
+      <ul>
+        {displayedUsers.map(({ node }) => {
+          return (
+            <li key={node.id}>{node.nicename}</li>
+          )
+        })}
+      </ul>
     </AuthorizedContent>
   );
 }
