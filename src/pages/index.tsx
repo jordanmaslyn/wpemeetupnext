8a528e4d9554b405475fc8a4eb03ae8fbdca5561
Jordan Maslyn <jordan.maslyn@wpengine.com>
2021-07-15 23:42:02 -0400

    Add title to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { UsersMap } from 'components/maps/UsersMap';
 import Layout from "components/Layout"
+import Head from 'next/head';
 
 export default function Page() {
   
 
   return (
     <Layout>
+      <Head>
+        <title>WP Engine Meetup</title>
+      </Head>
       <UsersMap />
     </Layout>
   );
