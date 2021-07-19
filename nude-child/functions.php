baa4472c6a2f58c6890b03cfd3d1107080b1959b
Alex Moon <alex.jared.moon@gmail.com>
2021-07-19 13:24:31 -0600

    feat: filter user registration by email domain

--- nude-child/functions.php
+++ nude-child/functions.php
@@ -1,9 +1,24 @@
 <?php
 
+# Set Google Maps API Key
 function my_acf_google_map_api( $api ){
     $api['key'] = GOOLE_MAPS_API_KEY;
     return $api;
 }
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+
+# Filter User registration by Email domain
+add_filter( 'registration_errors', 'myplugin_registration_errors', 10, 3 );
+function myplugin_registration_errors( $errors, $sanitized_user_login, $user_email ) {
+
+  if (! preg_match('/^[a-zA-Z0-9._\-+]+@wpengine\.com$/', $user_email )) {
+    $errors->add( 'invalid_email', __( 'ERROR: Only valid "wpengine.com" email address is allowed.' ));
+    $user_email = '';
+  }
+
+  return $errors;
+}
+
 
 ?>
