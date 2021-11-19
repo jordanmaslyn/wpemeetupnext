7f214829601771718fe8a5995b1d62a462be4daf
Alex Moon <alex.jared.moon@gmail.com>
2021-11-18 17:38:13 -0800

    chore: upgrade deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "faust.config";
 import React from "react";
-import { HeadlessProvider } from "@faustjs/next";
+import { FaustProvider } from "@faustjs/next";
 import { client } from "client";
 import type { AppProps } from "next/app";
 
@@ -10,11 +10,11 @@ import "../styles/globals.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <HeadlessProvider client={client} pageProps={pageProps}>
+      <FaustProvider client={client} pageProps={pageProps}>
         <AuthorizedContent>
           <Component {...pageProps} />
         </AuthorizedContent>
-      </HeadlessProvider>
+      </FaustProvider>
     </>
   );
 }
